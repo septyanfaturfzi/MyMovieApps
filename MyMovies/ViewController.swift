803bbfaac06242d3bf653642f374a1fa8388fc21
Septyan Fatur Fauzi <fatur.fauzi@gmail.com>
2022-11-16 09:20:14 +0700

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by NCS Pro on 16/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
